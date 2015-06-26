41d5d1a1c85a6de152843d9687f6a1798e258a7a
Greg Lapsley <rgreglapsley@yahoo.com>
2015-06-26 00:13:10 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.lili>li').click(function(){
+  	$(this).toggleClass('toggle');
+
+  });
+
+});
+
+
+
+$(document).ready(function(){
+	$('button').click(function(){
+		$('img').toggle();
+	});
+});
